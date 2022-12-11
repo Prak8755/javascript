38078daedd5b779d76a848edb6cfeeb140a67f02
Prakash Singh <prakashsingh@Prakashs-MacBook-Air.local>
2022-12-11 11:29:23 +0530

    js intro

---
+++ js1.js
@@ -0,0 +1,36 @@
+// JAVA SCRIPT WAS FOUND IN 1995 BY BREDNDON EICH IN 1995. 
+// MOCHA-- LIVE SCRIPT --JS.
+// 1995-- 1996 --------TILL DATE .
+
+// BASICS OF PROGRAMMING LANGUAGE
+
+// 1-VALUES OR DATA TYPES 
+// 2-VARIABLES 
+// 3- OPERATORS
+// 4-DECISONS
+// 5-LOOPS
+// 6-FUNCTIONS
+
+// A- DATA TYPE OR VALUES--- There are four types of data types 
+// a- Number data type , eg- 1, 2, 10
+// b-string data type , eg- " text" or 'text'
+// c- Boolean data type, True or False
+// d- Null or undefined data type 
+
+// VARIABLES
+
+// TO PRINT A MESSAGE 
+
+console.log("hello world");
+
+// There are three types of variables 
+
+// a- var 
+// b- let
+// c- const
+
+var a=20;
+let b='hello';
+const c=1234;
+console.log(`first number is ${a} also ${b} and last one is ${c}`);
+
