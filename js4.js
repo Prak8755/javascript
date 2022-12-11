200c82bb4394def080a9bf1d5654bccbba0392e6
Prakash Singh <prakashsingh@Prakashs-MacBook-Air.local>
2022-12-11 19:49:55 +0530

    new file

---
+++ js4.js
@@ -0,0 +1,20 @@
+var fullName= "PRAKASH";
+let email= 'Abc@gmail.com';
+var password='123456';
+const uid="1234";
+const mobile_no='123456789';
+const country='India';
+const state='Uttrakhand';
+const city='Dehradun';
+
+console.log(`
+user full name is:${fullName}
+his email id is :${email}
+password is :${password}
+uid is :${uid}
+and ten digit mobile number is :${mobile_no}
+user lives in :${city}
+district of :${state}
+and country is:${country}
+
+`)
