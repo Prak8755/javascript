3a2c816c2c2c67cb47df529ae25f6c8074f74f76
Prakash Singh <prakashsingh@Prakashs-MacBook-Air.local>
2022-12-11 11:44:54 +0530

    operators

---
+++ js2.js
@@ -0,0 +1,12 @@
+// There are multiple ways to print a message 
+
+var a=20;
+let b='prakash';
+const c=1223456;
+console.log('value of a is ',a);
+console.log("value of b is "+ b);
+console.log(`value of c is ${c}`);
+
+// if we want to print the message together
+
+console.log(`value of a is ${a} value of b is ${b} , value of c is ${c}`);
---
+++ js3.js
@@ -0,0 +1,14 @@
+// All about operators- we us follwing operators in JS.
+
+//  (+), (-), (*), (/), (%)
+
+// 1- ASSIGNMENT OPERATOR 
+
+let a =20;
+let b=10;
+let c=a+b;
+let d=a-b;
+let e=a*b;
+let f=a/b;
+let g=a%b;
+console.log(`addition is ${c},subs is ${d}, mult is ${e}, div is ${f}, reminder is ${g}`);
