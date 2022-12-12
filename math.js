4660e84a20557dbf8145829d0314dd972cabe38c
Prakash Singh <prakashsingh@Prakashs-MacBook-Air.local>
2022-12-12 20:11:46 +0530

    math added

---
+++ math.js
@@ -0,0 +1,17 @@
+const PI =Math.PI;
+console.log(PI);
+
+console.log(Math.round(PI));
+console.log(Math.round(9.45666));
+console.log(Math.floor(9.6));
+console.log(Math.ceil(9.4));
+console.log(Math.min(10,1,4,5,23,24));
+console.log(Math.max(23,34,1,34,67));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.round(Math.random()*10));
+
+console.log(Math.pow(3,4));
+console.log(Math.sin(90));
+
